fa5c322db694dd79e5a4266979084c2f83a46fa6
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2024-01-13 12:17:28 -0500

    Update default files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import AnimationFadeIn from "../components/AnimationFadeIn";
+
+const Home = () => {
+  return (
+    <AnimationFadeIn>
+      <div>Home</div>
+    </AnimationFadeIn>
+  );
+};
+
+export default Home;
